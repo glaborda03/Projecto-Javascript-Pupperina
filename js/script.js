d6639cffa1bebd95b25943c8dec68a5b871525d5
Gabriel Laborda <gabriel.laborda03@gmail.com>
2022-07-07 23:22:07 -0300

    borrando imagenes

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ const productos = [
   {
     id: 13,
     Articulos: "Cama",
-    img: "imagenes/foto.jpg",
+    img: "imagenes/cama.jpg",
     Precio: 1400,
     Descr: "Camas personalizadas, desmontables y lavables.",
     Tamaño: "S"
@@ -122,7 +122,7 @@ const productos = [
   {
     id: 14,
     Articulos: "Cama",
-    img: "imagenes/foto.jpg",
+    img: "imagenes/cama.jpg",
     Precio: 1600,
     Descr: "Camas personalizadas, desmontables y lavables.",
     Tamaño: "M"
@@ -131,7 +131,7 @@ const productos = [
   {
     id: 15,
     Articulos: "Cama",
-    img: "imagenes/foto.jpg",
+    img: "imagenes/cama.jpg",
     Precio: 1800,
     Descr: "Camas personalizadas, desmontables y lavables.",
     Tamaño: "L"
@@ -140,7 +140,7 @@ const productos = [
   {
     id: 16,
     Articulos: "Cama",
-    img: "imagenes/foto.jpg",
+    img: "imagenes/cama.jpg",
     Precio: 2000,
     Descr: "Camas personalizadas, desmontables y lavables.",
     Tamaño: "XL"
